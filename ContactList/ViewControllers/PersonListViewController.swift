5b8673ec41e88ecee23f75f65adc23d81f728c70
Julia Kazarina <120021628+kazarinaJu@users.noreply.github.com>
2023-04-19 16:24:30 +0500

    UITableViewDataSource

--- ContactList/ViewControllers/PersonListViewController.swift
+++ ContactList/ViewControllers/PersonListViewController.swift
@@ -8,22 +8,32 @@
 import UIKit
 
 final class PersonsListViewController: UITableViewController {
-
+    
+    private var contactList = Person.getPersons()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-
-
-//MARK: - Navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-    }
-    
 }
 
+//MARK: - Navigation
+//    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+//    }
+    
+
+    
 //MARK: - UITableViewDataSource
 extension PersonsListViewController {
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-       
-        return 0
-    }
+            contactList.count
+        }
+        
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "contact", for: indexPath)
+            var content = cell.defaultContentConfiguration()
+            let person = contactList[indexPath.row]
+            content.text = person.fullName
+            cell.contentConfiguration = content
+            return cell
+        }
 }
