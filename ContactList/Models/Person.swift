a5bca158e9b1670aff690815fc22e48cb298bf46
Julia Kazarina <120021628+kazarinaJu@users.noreply.github.com>
2023-04-19 16:23:54 +0500

    changed model

--- ContactList/Models/Person.swift
+++ ContactList/Models/Person.swift
@@ -15,18 +15,20 @@ struct Person {
         "\(name) \(surname)"
     }
     
-    static func getPerson() -> Person {
+    static func getPersons() -> [Person] {
         let dataStore = DataStore()
-        let nameIndex = Int.random(in: 0..<dataStore.names.count)
-        let surnameIndex = Int.random(in: 0..<dataStore.surnames.count)
-        let phoneIndex = Int.random(in: 0..<dataStore.phones.count)
-        let emailIndex = Int.random(in: 0..<dataStore.emails.count)
         
-        return Person(
-            name: dataStore.names[nameIndex],
-            surname: dataStore.surnames[surnameIndex],
-            phone: dataStore.phones[phoneIndex],
-            email: dataStore.emails[emailIndex]
-        )
+        var indices = Array(0..<dataStore.names.count)
+        indices.shuffle()
+        
+        let persons = indices.map{
+            index in Person(
+                name: dataStore.names[index],
+                surname: dataStore.surnames[index],
+                phone: dataStore.phones[index],
+                email: dataStore.emails[index]
+            )
+        }
+        return persons
     }
 }
